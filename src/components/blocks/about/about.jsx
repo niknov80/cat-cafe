4c6f8873f6b6157021b724f8bc12396333559999
niknov80 <43949474+niknov80@users.noreply.github.com>
2023-05-16 16:14:29 +0300

    Initial commit

---
+++ src/components/blocks/about/about.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Title, { TitleLevel, TitleSize } from "/src/components/ui/title/title";
+import { Address, Text, StyledSection, WorkTime, TextWrapper } from "./styles";
+
+function About() {
+  return (
+    <StyledSection>
+      <TextWrapper>
+        <Title level={TitleLevel.H1} size={TitleSize.BIG}>
+          Первое в России котокафе
+        </Title>
+        <Text>
+          Крупнейшее котокафе России, в котором живёт 50 кошек и котов, и каждый
+          из них ищет новый дом. Животных можно гладить, фотографировать, играть
+          с ними.
+        </Text>
+        <WorkTime>Рабочее время с 8:00 до 23:00</WorkTime>
+        <Address>Санкт-Петербург, Большая Конюшенная улица, 27</Address>
+      </TextWrapper>
+    </StyledSection>
+  );
+}
+
+export default About;
